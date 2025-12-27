0ebf9d9d45d19b0b42d884eae762738a047e45c4
Raiwin <raiwin.ko@gmail.com>
2025-12-27 23:37:35 +0700

    lab11_1_fixedLeaW

--- lab11_1.cpp
+++ lab11_1.cpp
@@ -1,2 +1,19 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include<iostream>
+#include<string>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+    int random(){
+       
+        return rand()%9;
+    } 
+    int main(){
+         srand(time(0));
+        cout << "Press Enter 3 times to reveal your future.\n";
+            for(int i=0; i < 3;i++){
+            cin.get();
+        }
+        string garde[9] = {"A","B+","B","C+","C","D+","D","F","W"};
+        cout << "You will get " << garde[random()] << " in this 261102.";
+
+    }
